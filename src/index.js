d181b5751c6486e187fe7adc01896584e715c9ec
ale-aleksandra <aleksandra99j@gmail.com>
2024-03-02 11:08:02 +0000

    Update API context

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generatePoem(event) {
   let apiKey = "739473ae0tafe02b875bc88cd82o1460";
   let prompt = `User instructions are: Generate a poem about ${keyword.value}.`;
   let context =
-    "Generate a four-line poem in basic HTML and separate each line with a <br />. The text should ALWAYS be 16px, italic and black. Make sure to follow user instructions. The poem DOESN'T have a title. Sign the poem at the end with 'SheCodes AI' inside a <strong> element.";
+    "Generate a four-line poem in basic HTML and separate each line with a <br />. The text should ALWAYS be 16px, italic and black. Make sure to follow user instructions. The poem DOESN'T have a title. Sign the poem at the end, on a new line, with 'SheCodes AI' inside a <strong> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating a poem...");
